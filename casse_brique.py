fa0c14b800f4505145c0969d6c8dc8947b617c9d
AtariAmiga <atarmigatar@gmail.com>
2018-11-10 17:39:37 +0100

    enfant: une balle s'affiche immobile

--- casse_brique.py
+++ casse_brique.py
@@ -1,11 +1,29 @@
 import pygame
+COULEUR_NOIR = (0, 0, 0)
+COULEUR_BLANC = (255, 255, 255)
 
 class Terrain(object):
-    pass
+    def __init__(self, x0, y0, largeur, hauteur ):
+        self.x0 = x0
+        self.y0 = y0
+        self.largeur = largeur
+        self.hauteur = hauteur
 
 
 class Balle(object):
-    pass
+    def __init__(self):
+        self.x = 50
+        self.y = 60
+        self.vx = 1
+        self.vy = 1
+        self.rayon = 10
+
+    def dessine_toi(self, fenetre):
+        pygame.draw.circle(fenetre, COULEUR_NOIR, [self.x, self.y], self.rayon)
+        pass
+
+    def bouge(self, terrain):
+        pass
 
 class MoteurDeJeu(object):
     def __init__(self, titre, largeur_fenetre, hauteur_fenetre):
@@ -17,7 +35,7 @@ class MoteurDeJeu(object):
         pygame.display.set_caption(titre)
         self.horloge = pygame.time.Clock()
 
-    def fait_ton_travail(self):
+    def fait_ton_travail(self, terrain, balle):
         le_jeu_tourne = True
         while le_jeu_tourne:
             tous_les_evenements = pygame.event.get()
@@ -27,14 +45,21 @@ class MoteurDeJeu(object):
                     pygame.quit()
                     quit()
 
+            self.fenetre.fill(COULEUR_BLANC)
+
+            balle.bouge(terrain)
+            balle.dessine_toi(self.fenetre)
+
+            pygame.display.update()
+            self.horloge.tick(self.FPS)
 
 def boucle_de_jeu():
     le_moteur = MoteurDeJeu('Casse brique', largeur_fenetre = 800, hauteur_fenetre = 600)
 
-    le_terrain = Terrain()
+    le_terrain = Terrain(x0=0, y0=0, largeur=800, hauteur=600)
     la_balle = Balle()
 
-    le_moteur.fait_ton_travail()
+    le_moteur.fait_ton_travail(le_terrain, la_balle)
 
 if __name__ == '__main__':
     boucle_de_jeu()
