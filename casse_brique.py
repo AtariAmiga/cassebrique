86ad5861049b197ad9ff27f7c3d80f99c36d4b2c
AtariAmiga <atarmigatar@gmail.com>
2018-11-10 17:09:56 +0100

    enfant: la fenêtre s'affiche et on peut la fermer

--- casse_brique.py
+++ casse_brique.py
@@ -17,12 +17,24 @@ class MoteurDeJeu(object):
         pygame.display.set_caption(titre)
         self.horloge = pygame.time.Clock()
 
+    def fait_ton_travail(self):
+        le_jeu_tourne = True
+        while le_jeu_tourne:
+            tous_les_evenements = pygame.event.get()
+
+            for evenement in tous_les_evenements:
+                if evenement.type == pygame.QUIT: # C'est le bouton X sur la fenêtre
+                    pygame.quit()
+                    quit()
+
 
 def boucle_de_jeu():
     le_moteur = MoteurDeJeu('Casse brique', largeur_fenetre = 800, hauteur_fenetre = 600)
 
     le_terrain = Terrain()
     la_balle = Balle()
+
+    le_moteur.fait_ton_travail()
 
 if __name__ == '__main__':
     boucle_de_jeu()
